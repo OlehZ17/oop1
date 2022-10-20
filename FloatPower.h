253506b9a7c129e0d650ce8be8daf3d7bb5008c0
OlehZ17 <91013333+OlehZ17@users.noreply.github.com>
2022-10-20 20:27:11 +0300

    Update FloatPower.h

--- FloatPower.h
+++ FloatPower.h
@@ -8,10 +8,10 @@ private:
 public:
 	double GetFirst() { return First; }
 	double GetSecond() { return Second; }
-	void SetFirst(int value);
-	void SetSecond(int value);
-	double Init(int x, int y);
+	void SetFirst(double value);
+	void SetSecond(double value);
+	double Init(double x, double y);
 	void Display() const;
 	void Read();
-	double power(int x, int y);
+	double power(double x, double y);
 };
